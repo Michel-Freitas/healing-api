074b3c07021a159520bf4f173fee89b936ba426f
Michel Freitas Santos <michel_freitas@virgo.atlantico.net.br>
2023-03-02 01:29:48 -0300

    Starting Project

---
+++ src/main/java/com/example/healingapi/HealingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.healingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealingApiApplication.class, args);
+	}
+
+}
